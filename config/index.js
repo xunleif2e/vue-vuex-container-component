ecab7e4e3a649943d5524f4b5a7de3bff5853943
赵兵 <zhaobing@xunlei.com>
2018-05-02 17:49:08 +0800

    fix(demo): 修复demo页面无法显示的问题

--- config/index.js
+++ config/index.js
@@ -49,8 +49,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'vue-vuex-container-component/dist/static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
